55e66f02c191a5f1630a96b2a5086549ab20b996
kristru <krissafuentes@gmail.com>
2021-12-29 14:48:58 -0700

    test

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
+ const toDos = [];
+ const  addToDoForm = document.getElementById('addToDoForm');
+
+
+ function createNewToDo() {
+   const newToDoText = document.getElementById('newToDoText');
+   if (!newToDoText.value) {return}
+
+   toDos.push({
+     title:newToDoText.value,
+     complete: false
+   });
+
+   newToDoText.value = '';
+
+   renderTheUI(toDos);
+ }
+
+addToDoForm.addEventListener('submit', event =>{
+  event.preventDefault();
+  createNewToDo();
+});
+
+function renderTheUI(toDos){
   const toDoList = document.getElementById('toDoList');
+
 
   addToDoForm.addEventListener('submit', event => {
     // prevent 'submit' from refreshing the page(default behavior)
@@ -43,5 +66,34 @@ function onReady() {
 
 window.onload = function() {
 //  alert("The window has loaded!");
+
+  toDoList.textContent = '';
+
+  toDos.forEach(function(toDo){
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const deleteBtn = document.createElement('button');
+    checkbox.type = "checkbox";
+    deleteBtn.textContent = ('Delete Task');
+
+    newLi.textContent = toDo.title;
+
+    toDoList.appendChild(newLi);
+    newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', event =>{
+      event.preventDefault();
+      newLi.remove('li');
+    });
+
+    });
+};
+
+
+};
+
+
+window.onload = function() {
   onReady();
 };
